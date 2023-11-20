40b2b67c1a43145f7e8d82bc59285cf84c7756a2
Donel Chihoma <dchihoma8@gmail.com>
2023-11-20 11:46:34 +0200

    Question 6 Complete

--- Question 6/fractional_nft_algorand.py
+++ Question 6/fractional_nft_algorand.py
@@ -1,12 +1,10 @@
-# from utils import get_accounts, get_algod_client
+# Importing necessary modules
 from algosdk import account, mnemonic
 from algosdk import transaction
 from typing import Dict, Any
 import json
 from base64 import b64decode
 import math
-
-from algosdk import transaction
 from algosdk.v2client import algod
 
 
@@ -16,22 +14,24 @@ algod_token = ""
 algod_client = algod.AlgodClient(algod_token, algod_address)
 
 
-def generate_algorand_account():
-    private_key, address = account.generate_account()
-    return private_key, address
+# def generate_algorand_account():
+#     private_key, address = account.generate_account()
+#     return private_key, address
 
-for _ in range(3):
-    private_key, address = generate_algorand_account()
-    print(f"Generated account - Address: {address}, Private Key: {private_key}")
+# for _ in range(3):
+#     private_key, address = generate_algorand_account()
+#     print(f"Generated account - Address: {address}, Private Key: {private_key}")
 
-addr1 = "NK3KOECJ3WYJWCGA2TGMOIGGGJTR6JMBBHRZL5EJN2X73TM22NMOC2XF3A"
-pk1 = "i+wzKA+H7qjuKhVbi8ym3+1NivNPtVhqjhOgAOscrFZqtqcQSd2wmwjA1MzHIMYyZx8lgQnjlfSJbq/9zZrTWA=="
+# 3 Algorand Account information used for the exam
 
-addr2 = "DTQKRDVSCESTTSE5CYD3RBFZZBCINJR42URWNYTH2GUEXSRGRU3XMLWI3Y"
-pk2 = "Vgbu4qDHKjNG+goe+92vKtO+m2NG7NGZ1O4YIL2I9mkc4KiOshElOcidFge4hLnIRIamPNUjZuJn0ahLyiaNNw=="
+addr1 = "N73LTWPZRQ4QN4JQ2ED2IIIUGKSR23BMTNNUCIVFUHIMGOJ57BA2TUTPYU"
+pk1 = "WzlIWEnmpGAkcREIjAufYsIO8Db0mEzWeMDq4QpBHnJv9rnZ+Yw5BvEw0QekIRQypR1sLJtbQSKlodDDOT34QQ=="
 
-addr3 = "NK3KOECJ3WYJWCGA2TGMOIGGGJTR6JMBBHRZL5EJN2X73TM22NMOC2XF3A"
-pk3 = "i+wzKA+H7qjuKhVbi8ym3+1NivNPtVhqjhOgAOscrFZqtqcQSd2wmwjA1MzHIMYyZx8lgQnjlfSJbq/9zZrTWA=="
+addr2 = "NCQ3ZZ5XW7NME72TFQFMEQU6ENNPMYPHZ5BZVMKCLBTMHKXGGE3LRLF3HA"
+pk2 = "7UHdXnTVWb/7hCpbBp+L24JY22QBfeE5zP3nAQYyYstoobznt7fawn9TLArCQp4jWvZh589DmrFCWGbDquYxNg=="
+
+addr3 = "RBZESUKPW4CVAQ5CY4IGOQJFSDO5NFKZXWUOFXRECZEBDNODKMYW7VGGJM"
+pk3 = "uCylsc7qnbkFoj5fr4sHa1mGxjCL3z4MGKcZ/Bhe6waIcklRT7cFUEOixxBnQSWQ3daVWb2o4t4kFkgRtcNTMQ=="
 
 
 
@@ -39,15 +39,20 @@ mn1 = mnemonic.from_private_key(pk1)
 mn2 = mnemonic.from_private_key(pk2)
 mn3 = mnemonic.from_private_key(pk3)
 
-# User B creates an asset UCTZAR with a total supply
-# 10 units and set itself to the freeze/clawback/manager/reserve roles
+# Account 1 creates a fractional NFT FRANFT with a total supply of 10 units
+
 suggested_params = algod_client.suggested_params()
+
+# To create a fractional NFT the total should be a a number that is a power of 10 but greater that 1
+# The decimals should be a log of base 10 of the total number of units
+# This ensures that the total units always equal to 1
+
 fractional_nft_creation = transaction.AssetConfigTxn(
     sender=addr1,
     sp=suggested_params,
     default_frozen=False,
-    unit_name="Donel_NFT",
-    asset_name="Donel_NFT",
+    unit_name="FRANFT",
+    asset_name="FRANFT",
     manager=addr1,
     reserve=addr1,
     freeze=addr1,
@@ -57,40 +62,59 @@ fractional_nft_creation = transaction.AssetConfigTxn(
     decimals= math.log10(10),
 )
 
-# Sign with secret key of creator
+# Sign account creation with private key of creator, account 1
 stxn_fractional_nft = fractional_nft_creation.sign(pk1)
-# Send the transaction to the network and retrieve the txid.
+
+
+# Sending the transaction to the network and retrieving the transaction ID.
 fractional_ASA_txid = algod_client.send_transaction(stxn_fractional_nft)
 print(f"Sent asset create transaction with txid: {fractional_ASA_txid}")
-# Wait for the transaction to be confirmed
+
+
+# Waiting for the transaction to be confirmed
 fractional_ASA_results = transaction.wait_for_confirmation(algod_client, fractional_ASA_txid, 4)
 print(f"Result confirmed in round: {fractional_ASA_results['confirmed-round']}")
 
-# grab the asset id for the asset we just created
+# Grab the asset id for the Fractional NFT we just created
 fractional_nft_asset = fractional_ASA_results["asset-index"]
 print(f"Asset ID created: {fractional_nft_asset}")
 
+
+# Opt-in transactions for addr2 and addr3
+# Account 2 opting in
 optin_2 = transaction.AssetOptInTxn(
     sender=addr2, sp=suggested_params, index=fractional_nft_asset
 )
+
+# Account 2 signing the opting in transaction
 signed_optin_2_txn = optin_2.sign(pk2)
+
+# Opt in transaction sent to the network to retrieve the Transaction ID
 optin_2_txid = algod_client.send_transaction(signed_optin_2_txn)
 print(f"Sent opt in transaction with txid: {optin_2_txid}")
 
+# Waiting for the transaction to be confirmed
 optin_2_results = transaction.wait_for_confirmation(algod_client, optin_2_txid, 4)
 print(f"Result confirmed in round: {optin_2_results['confirmed-round']}")
 
+
+# Account 3 opting in
 optin_3 = transaction.AssetOptInTxn(
     sender=addr3, sp=suggested_params, index=fractional_nft_asset
 )
-signed_optin_3_txn = optin_2.sign(pk3)
+
+# Account 3 signing the opting in transaction
+signed_optin_3_txn = optin_3.sign(pk3)
+
+# Opt in transaction sent to the network to retrieve the Transaction ID
 optin_3_txid = algod_client.send_transaction(signed_optin_3_txn)
 print(f"Sent opt in transaction with txid: {optin_3_txid}")
 
-
+# Waiting for the transaction to be confirmed
 optin_3_results = transaction.wait_for_confirmation(algod_client, optin_3_txid, 4)
 print(f"Result confirmed in round: {optin_3_results['confirmed-round']}")
 
+# Asset transfer transactions from account 1 to account(s) 2 & 3
 txn_2 = transaction.AssetTransferTxn(
     sender=addr1,
     sp=suggested_params,
@@ -101,18 +125,51 @@ txn_2 = transaction.AssetTransferTxn(
 
 
 txn_3 = transaction.AssetTransferTxn(
-    sender=addr3,
+    sender=addr1,
     sp=suggested_params,
-    receiver=addr1,
+    receiver=addr3,
     amt=3,
     index=fractional_nft_asset,
 )
 
 
-def check_nft_ownership(client, asset_id, recipients):
-    for private_key, address in recipients:
-        balance = AlgorandUtils.get_asset_balance(client, address, asset_id)
+# Signing and sending asset transfer transactions
+stxn_2 = txn_2.sign(pk1)
+stxn_3 = txn_3.sign(pk1)
+
+stxn_2_txid = algod_client.send_transaction(stxn_2)
+print(f"Sent asset transfer to Address 2 transaction with txid: {stxn_2_txid}")
+
+# Wait for the transaction to be confirmed
+stxn_2_results = transaction.wait_for_confirmation(algod_client, stxn_2_txid, 4)
+print(f"Result confirmed in round: {stxn_2_results['confirmed-round']}")
+
+
+stxn_3_txid = algod_client.send_transaction(stxn_3)
+print(f"Sent asset transfer to Address 3 transaction with txid: {stxn_3_txid}")
+
+# Wait for the transaction to be confirmed
+stxn_3_results = transaction.wait_for_confirmation(algod_client, stxn_3_txid, 4)
+print(f"Result confirmed in round: {stxn_3_results['confirmed-round']}")
+
+
+
+# List of holders with their private keys and addresses
+holders = [(pk1, addr1),
+              (pk2, addr2),
+              (pk3, addr3),
+              ]
+
+# Function to check ownership of the fractional NFT for each holder
+def check_nft_ownership(asset_id, holders):
+    for private_key, address in holders:
+        account_asset_info = algod_client.account_asset_info(address,asset_id)
+        balance = account_asset_info.get('asset-holding', {}).get('amount', None)
         if balance > 0:
             print(f"Address {address} owns {balance} units of the fractional NFT.")
         else:
             print(f"Address {address} does not own any units of the fractional NFT.")
+
+
+# Check ownership for each holder
+check_nft_ownership(fractional_nft_asset, holders)
